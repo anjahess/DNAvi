df06ee11e12980daf83913f5b290fda1b07ac564
anjahess <anja.hess@mail.de>
2025-11-05 09:21:19 +0100

    be more flexible in accepting paths to directories

--- DNAvi.py
+++ DNAvi.py
@@ -133,6 +133,8 @@ if args.config:
 # Decide: folder or single file processing
 #########################################################################
 if os.path.isdir(csv_path):
+    if not csv_path.endswith("/"):
+        csv_path = f"{csv_path}/"
     print(f"--- Checking folder {csv_path}")
     files_to_check = [f"{csv_path}{e}" for e in os.listdir(csv_path) if
                       e.endswith(tuple(ACCEPTED_FORMATS))]
